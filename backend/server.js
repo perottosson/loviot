7fd09eb283d968804cb9d14ecfcd339022f04fef
Per Ottosson <per.ottosson.gbg@gmail.com>
2020-06-29 15:50:42 +0200

    update

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const port = new SerialPort('COM3', { baudRate: 9600 });
 const parser = port.pipe(new Readline({ delimiter: '\n' }));
 // Read the port data
 port.on("open", () => {
-  console.log('serial port open');
+  console.log('serial port open ');
 });
 
 io.sockets.on('connection', (socket) => {
